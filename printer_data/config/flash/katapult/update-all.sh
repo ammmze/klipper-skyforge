de462c162f97f7826e2e95aea026936dd104f83b
klipper <orangepi@skyforge-de4774c>
2024-11-27 02:51:51 +0000

    update-all.sh modified - 11/27/2024 - 02:51:47 AM

--- printer_data/config/flash/katapult/update-all.sh
+++ printer_data/config/flash/katapult/update-all.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-sudo service klipper stop
-cd ~/klipper
-git pull
+#sudo service klipper stop
+#cd ~/klipper
+#git pull
 
-export KCONFIG_CONFIG=m8p_v2.config
-make clean
-make menuconfig
-make
-read -p "Manta M8P v2.0 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+#export KCONFIG_CONFIG=m8p_v2.config
+#make clean
+#make menuconfig
+#make
+#read -p "Manta M8P v2.0 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
 # ./scripts/flash-sdcard.sh /dev/ttyAMA0 fysetc-spider-v1
 # read -p "Spider firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
@@ -20,4 +20,4 @@ read -p "Manta M8P v2.0 firmware built, please check above for any errors. Press
 # read -p "RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 # make flash KCONFIG_CONFIG=config.rpi
 
-sudo service klipper start
+#sudo service klipper start
