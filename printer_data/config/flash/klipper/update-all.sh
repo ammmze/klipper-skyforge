987e3ba583198e426006ff942f96f4770e456f3a
klipper <orangepi@skyforge-de4774c>
2024-11-27 04:50:55 +0000

    update-all.sh modified - 11/27/2024 - 04:50:51 AM

--- printer_data/config/flash/klipper/update-all.sh
+++ printer_data/config/flash/klipper/update-all.sh
@@ -9,9 +9,10 @@ touch ~/printer_data/config/flash/klipper/"${KCONFIG_CONFIG}"
 ln -sf ~/printer_data/config/flash/klipper/"${KCONFIG_CONFIG}" "${KCONFIG_CONFIG}"
 make clean KCONFIG_CONFIG="${KCONFIG_CONFIG}"
 make menuconfig KCONFIG_CONFIG="${KCONFIG_CONFIG}"
-make KCONFIG_CONFIG="${KCONFIG_CONFIG}"
+make -j "$(nproc --all)" KCONFIG_CONFIG="${KCONFIG_CONFIG}"
 read -p "Manta M8P v2.0 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
+# sudo dfu-util -a 0 -d 0483:df11 --dfuse-address 0x08020000 -D ~/klipper/out/klipper.bin
 # ./scripts/flash-sdcard.sh /dev/ttyAMA0 fysetc-spider-v1
 # read -p "Spider firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
