c543d04bfe6be2bf4ce3d48b1919754c03dce769
klipper <orangepi@skyforge-de4774c>
2024-11-27 05:16:06 +0000

    update-all.sh modified - 11/27/2024 - 05:16:03 AM

--- printer_data/config/flash/katapult/update-all.sh
+++ printer_data/config/flash/katapult/update-all.sh
@@ -10,14 +10,20 @@ if [ -z "${MCUS}" ]; then
   exit 1
 fi
 
+function build {
+  local mcu="$1"
+  local label="$2"
+  local config="${mcu}.config"
+  touch ~/printer_data/config/flash/katapult/"${config}"
+  ln -sf ~/printer_data/config/flash/katapult/"${config}" "${config}"
+  make clean KCONFIG_CONFIG="${config}"
+  make menuconfig KCONFIG_CONFIG="${config}"
+  make -j "$(nproc --all)" KCONFIG_CONFIG="${config}"
+  read -p "${label} bootloader built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+}
+
 function update_m8p_v2 {
-  export KCONFIG_CONFIG=m8p_v2.config
-  touch ~/printer_data/config/flash/katapult/"${KCONFIG_CONFIG}"
-  ln -sf ~/printer_data/config/flash/katapult/"${KCONFIG_CONFIG}" "${KCONFIG_CONFIG}"
-  make clean KCONFIG_CONFIG="${KCONFIG_CONFIG}"
-  make menuconfig KCONFIG_CONFIG="${KCONFIG_CONFIG}"
-  make -j "$(nproc --all)" KCONFIG_CONFIG="${KCONFIG_CONFIG}"
-  read -p "Manta M8P v2.0 bootloader built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+  build m8p_v2 'Manta M8P v2.0'
   
   # TODO: Flash out/katapult.bin
   # sudo dfu-util --list
@@ -30,6 +36,19 @@ function update_m8p_v2 {
   sudo dfu-util -a 0 -D ~/katapult/out/katapult.bin --dfuse-address 0x08000000:force:leave -d 0483:df11
 }
 
+function update_sb2209 {
+  build sb2209 'SB2209'
+  
+  # sudo dfu-util --list
+  # Found DFU: [0483:df11] ver=0200, devnum=4, cfg=1, intf=0, path="5-1.4", alt=1, name="@Option Bytes   /0x5200201C/01*88 e", serial="3560376F3431"
+  # Found DFU: [0483:df11] ver=0200, devnum=4, cfg=1, intf=0, path="5-1.4", alt=0, name="@Internal Flash   /0x08000000/8*128Kg", serial="3560376F3431"
+  
+  while ! sudo dfu-util --list | grep -s '\[0483:df11\]'; do # TODO: Verify serial?
+    read -p 'SB2209 needs to be in DFU mode. Please hold Boot0 and press reset to enter DFU mode. Press [Enter] to try again, or [Ctrl+C] to abort'
+  done
+  #sudo dfu-util -a 0 -D ~/katapult/out/katapult.bin --dfuse-address 0x08000000:force:leave -d 0483:df11
+}
+
 sudo service klipper stop
 cd ~/katapult
 git pull
@@ -39,6 +58,9 @@ for mcu in ${MCUS}; do
   case "$mcu" in
     m8p_v2)
       update_m8p_v2
+      ;;
+    sb2209)
+      update_sb2209
       ;;
   esac
 done
